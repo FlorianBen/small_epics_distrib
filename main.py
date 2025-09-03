4ab96d308b89a5291194edd76dc65cee08098863
Florian Benedetti <florian.benedetti@cea.fr>
2025-09-03 17:05:57 +0200

    Add logging and concurrent clone.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import tomllib
 import subprocess
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
 from typing import List, Set
 from string import Template
+import logging
 
 WORK_DIR = "./"
+
+# Configuration du logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%H:%M:%S",
+)
 
 
 def main():
@@ -39,6 +48,7 @@ class EpicsDistrib:
         self.workdir: Path = Path(workdir)
         self.load_deps("cfg/dependencies.toml")
         self.load_conf(filepath)
+        self.max_worker = 8
         # self.area_detector = False
 
     def load_deps(self, filepath: Path):
@@ -68,6 +78,7 @@ class EpicsDistrib:
         for key in self.dict_conf["support"]:
             for mod in self.module_db:
                 if key == mod.name:
+                    logging.info("Module {} found".format(mod.name))
                     mod.to_build = True
                     if key == "areaDetector":
                         self.area_detector = True
@@ -78,9 +89,10 @@ class EpicsDistrib:
                     mod.to_build = True
 
     def create_workdir(self):
+        logging.info("Create {} folder".format(self.workdir))
         self.workdir.mkdir(parents=True, exist_ok=True)
         (self.workdir / "support").mkdir(parents=True, exist_ok=True)
-        (self.workdir / "extensions").mkdir(parents=True, exist_ok=True)
+        (self.workdir / "extension").mkdir(parents=True, exist_ok=True)
         for mod in self.module_db:
             if mod.type_dep == "base":
                 mod.path = self.workdir / "base"
@@ -88,11 +100,19 @@ class EpicsDistrib:
                 mod.path = self.workdir / Path(mod.type_dep) / mod.name
 
     def download_module(self):
+        logging.info("Download modules with git")
+        cmds = []
         for mod in self.module_db:
             if mod.to_build:
-                self.clone(mod)
+                cmds.append(self.create_git_cmd(mod))
                 for dep in mod.deps:
                     self.deps.add(dep)
+        results = []
+
+        with ThreadPoolExecutor(max_workers=self.max_worker) as executor:
+            futures = [executor.submit(self.run_cmd, cmd) for cmd in cmds]
+            for future in as_completed(futures):
+                results.append(future.result())
 
     def do_build(self):
         self.create_workdir()
@@ -103,22 +123,24 @@ class EpicsDistrib:
         if self.area_detector == True:
             self.create_ad_files()
 
-    def clone(self, mod: Dependency):
-        subprocess.run(
-            [
-                "git",
-                "clone",
-                "--recursive",
-                mod.url,
-                mod.path,
-                "-b",
-                mod.version,
-            ],
-            check=True,
-            stdout=subprocess.PIPE,
-        ).stdout
+    def create_git_cmd(self, mod: Dependency):
+        return [
+            "git",
+            "clone",
+            "--recursive",
+            mod.url,
+            mod.path,
+            "-b",
+            mod.version,
+        ]
+
+    def run_cmd(self, cmd):
+        logging.info("Run command {} for {}".format(cmd[0], cmd[3]))
+        result = subprocess.run(cmd, capture_output=True, text=True)
+        return cmd, result.returncode, result.stdout, result.stderr
 
     def create_template(self):
+        logging.info("Create RELEASE.local")
         self.release_template = Template(
             Path("template/RELEASE.local.template").read_text()
         ).substitute(EPICS_ROOT_PATH=self.workdir.absolute())
@@ -126,7 +148,7 @@ class EpicsDistrib:
         for mod in self.module_db:
             if mod.to_build:
                 if mod.type_dep == "extension":
-                    stri = "{0}={1}/{2}\n".format(mod.env, "$(EXTENSIONS)", mod.name)
+                    stri = "{0}={1}/{2}\n".format(mod.env, "$(EXTENSION)", mod.name)
                     self.release_template = self.release_template + stri
                 elif mod.type_dep == "support":
                     stri = "{0}={1}/{2}\n".format(mod.env, "$(SUPPORT)", mod.name)
@@ -140,11 +162,13 @@ class EpicsDistrib:
             "" if self.dict_conf["install"]["in-source"] == True else "INSTALL".format
         )
 
+        logging.info("Create CONFIG_SITE.local")
         self.config_template = Template(
             Path("template/CONFIG_SITE.local.template").read_text()
         ).substitute(shared=shared_str, static=static_str)
 
     def create_makefile(self):
+        logging.info("Create Makefile")
         make_str = ""
         for mod in self.module_db:
             if mod.to_build and mod.type_dep == "support" and mod.name != "base":
@@ -153,12 +177,29 @@ class EpicsDistrib:
                     if dep != "base":
                         make_str = make_str + "{} ".format(dep)
                 make_str = make_str + "\n"
+        make_str = (
+            make_str
+            + "all: "
+            + " ".join(
+                [
+                    mod.name
+                    for mod in self.module_db
+                    if (
+                        mod.to_build
+                        and mod.type_dep == "support"
+                        and mod.name != "base"
+                    )
+                ]
+            )
+            + "\n"
+        )
         self.makefile_template = Template(
             Path("template/Makefile.template").read_text()
         ).substitute(order=make_str)
         self.save_template(self.workdir / "support/Makefile", self.makefile_template)
 
     def create_ad_files(self):
+        logging.info("Create specific file for AreaDetector")
         ad_template = Template(
             Path("template/areaDetector/RELEASE_LIBS.local.template").read_text()
         ).substitute(EPICS_ROOT_PATH=self.workdir.absolute())
@@ -193,13 +234,13 @@ class EpicsDistrib:
             self.workdir / "support/RELEASE.local", self.release_template
         )
         self.save_template(
-            self.workdir / "extensions/RELEASE.local", self.release_template
+            self.workdir / "extension/RELEASE.local", self.release_template
         )
         self.save_template(
             self.workdir / "support/CONFIG_SITE.local", self.config_template
         )
         self.save_template(
-            self.workdir / "extensions/CONFIG_SITE.local", self.config_template
+            self.workdir / "extension/CONFIG_SITE.local", self.config_template
         )
 
 
